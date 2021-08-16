5f3f2d9c9886d063fc4f7bb0bc0873b6e796530d
darkbake-WGU <78395093+darkbake-WGU@users.noreply.github.com>
2021-08-16 15:50:29 -0700

    Add files via upload

--- explore_enron_data.py
+++ explore_enron_data.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+dd
 """ 
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
@@ -15,10 +15,113 @@
     
 """
 
+#Import Statements
 import pickle
+import  pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+# %matplotlib inline
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r")
 
-print(enron_data)
+#Load the data as a dictionary and then convert it to a data frame
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+data2 = pd.DataFrame.from_dict(enron_data)
+
+#look at how the data is formatted
+data2.head()
+
+#Checking out James Prentice to see his data
+data2["PRENTICE JAMES"]
+
+#Listing the POI in the data set
+for person_name in data2:
+    if data2[person_name]["poi"]==1:
+        print(person_name)
+
+#Checking how many e-mails were sent from a POI to Wesley Colwell
+data2["COLWELL WESLEY"]["from_poi_to_this_person"]
+
+# +
+#Checking out the data on Kenneth Lay, Jeffrey Skilling, and Andrew Fastow
+print("Kenneth Lay: \n", data2["LAY KENNETH L"], "\n")
+
+print("Jeffrey Skilling: \n", data2["SKILLING JEFFREY K"], "\n")
+
+print("Andrew Fastow: \n", data2["FASTOW ANDREW S"])
+
+
+# -
+
+def featureFormat( dictionary, features, remove_NaN=True, remove_all_zeroes=True, remove_any_zeroes=False, sort_keys = False):
+    """ convert dictionary to numpy array of features
+        remove_NaN = True will convert "NaN" string to 0.0
+        remove_all_zeroes = True will omit any data points for which
+            all the features you seek are 0.0
+        remove_any_zeroes = True will omit any data points for which
+            any of the features you seek are 0.0
+        sort_keys = True sorts keys by alphabetical order. Setting the value as
+            a string opens the corresponding pickle file with a preset key
+            order (this is used for Python 3 compatibility, and sort_keys
+            should be left as False for the course mini-projects).
+        NOTE: first feature is assumed to be 'poi' and is not checked for
+            removal for zero or missing values.
+    """
+
+
+    return_list = []
+
+    # Key order - first branch is for Python 3 compatibility on mini-projects,
+    # second branch is for compatibility on final project.
+    if isinstance(sort_keys, str):
+        import pickle
+        keys = pickle.load(open(sort_keys, "rb"))
+    elif sort_keys:
+        keys = sorted(dictionary.keys())
+    else:
+        keys = list(dictionary.keys())
+
+    for key in keys:
+        tmp_list = []
+        for feature in features:
+            try:
+                dictionary[key][feature]
+            except KeyError:
+                print("error: key ", feature, " not present")
+                return
+            value = dictionary[key][feature]
+            if value=="NaN" and remove_NaN:
+                value = 0
+            tmp_list.append( float(value) )
+
+        # Logic for deciding whether or not to add the data point.
+        append = True
+        # exclude 'poi' class as criteria.
+        if features[0] == 'poi':
+            test_list = tmp_list[1:]
+        else:
+            test_list = tmp_list
+        ### if all features are zero and you want to remove
+        ### data points that are all zero, do that here
+        if remove_all_zeroes:
+            append = False
+            for item in test_list:
+                if item != 0 and item != "NaN":
+                    append = True
+                    break
+        ### if any features for a given data point are zero
+        ### and you want to remove data points with any zeroes,
+        ### handle that here
+        if remove_any_zeroes:
+            if 0 in test_list or "NaN" in test_list:
+                append = False
+        ### Append the data point if flagged for addition.
+        if append:
+            return_list.append( np.array(tmp_list) )
+
+    return np.array(return_list)
+
+featureFormat(enron_data)
 
 
