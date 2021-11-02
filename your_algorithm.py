a23f98af9d8c0e4f1dea96006d8a669b4cc0e8b3
darkbake-WGU <78395093+darkbake-WGU@users.noreply.github.com>
2021-11-02 16:46:46 -0700

    Add files via upload

--- your_algorithm.py
+++ your_algorithm.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from time import time
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
@@ -7,6 +8,11 @@ from class_vis import prettyPicture
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.datasets import make_classification
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from sklearn.metrics import accuracy_score
+
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -32,32 +38,29 @@ plt.show()
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
-### visualization code (prettyPicture) to show you the decision boundary
+# ## your code here!  name your classifier object clf if you want the 
+# ## visualization code (prettyPicture) to show you the decision boundary
 
 #clf = KNeighborsClassifier(n_neighbors=3)
-clf = AdaBoostClassifier(n_estimators=100, random_state=0)
+#clf = AdaBoostClassifier(n_estimators=100, random_state=0)
+clf = RandomForestClassifier(max_depth=2, random_state=0)
+
 
 t0 = time()
 clf = clf.fit(features_train, labels_train)
 print("training time:", round(time()-t0, 3), "s")
 
 t0 = time()
-
-pred = clf.predict(features_test, labels_test)
-
+pred = clf.predict(features_test)
 print("prediction time:", round(time()-t0, 3), "s")
 
+
 acc: object = accuracy_score(pred, labels_test)
-
 print("Accuracy: ", acc)
-
-
-
-
-
 
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
     pass
+
+
