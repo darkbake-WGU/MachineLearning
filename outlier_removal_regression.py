47ce6cb11ab3ccf1b8d7a4f523a50e1e9d5295bd
darkbake-WGU <78395093+darkbake-WGU@users.noreply.github.com>
2021-11-21 19:19:33 -0800

    Add files via upload

--- outlier_removal_regression.py
+++ outlier_removal_regression.py
@@ -55,11 +55,13 @@ if len(cleaned_data) > 0:
     ages, net_worths, errors = list(zip(*cleaned_data))
     ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
     net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
+    ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages, net_worths, test_size=0.1,
+                                                                                random_state=42)
 
     ### refit your cleaned data!
     try:
-        reg.fit(ages, net_worths)
-        plt.plot(ages, reg.predict(ages), color="blue")
+        reg.fit(ages_train, net_worths_train)
+        plt.plot(ages_train, reg.predict(ages_train), color="blue")
     except NameError:
         print("you don't seem to have regression imported/created,")
         print("   or else your regression object isn't named reg")
@@ -68,7 +70,7 @@ if len(cleaned_data) > 0:
     plt.scatter(ages, net_worths)
     plt.xlabel("ages")
     plt.ylabel("net worths")
-    #plt.show()
+    plt.show()
 else:
     print("outlierCleaner() is returning an empty list, no refitting to be done")
 
