348e8582e3a9622e2ef62cd64f67bc1d35be0eec
darkbake-WGU <78395093+darkbake-WGU@users.noreply.github.com>
2022-11-17 19:03:59 -0800

    Add files via upload

--- outlier_cleaner.py
+++ outlier_cleaner.py
@@ -10,19 +10,11 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-
+    
     cleaned_data = []
-    data = []
-    n = 0
 
     ### your code goes here
 
-    for prediction, net_worth, age in zip(predictions, net_worths, ages):
-            data.append((age, net_worth, abs(prediction - net_worth)))
-
-
-    data.sort(key=lambda y: y[2])
-
-    cleaned_data = data[0:(81)]
-
+    
     return cleaned_data
+
--- outlier_removal_regression.py
+++ outlier_removal_regression.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import pickle
-
-import matplotlib.pyplot as plt
+import random
 import numpy
-from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import joblib
 
 from outlier_cleaner import outlierCleaner
+from sklearn.linear_model import LinearRegression
 
 ### load up some practice data with outliers in it
-ages = pickle.load( open("practice_outliers_ages.pkl", "rb") )
-net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "rb") )
+ages = joblib.load( open("./practice_outliers_ages.pkl", "rb") )
+net_worths = joblib.load( open("./practice_outliers_net_worths.pkl", "rb") )
 
 
 
@@ -23,12 +23,18 @@ net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
 from sklearn.model_selection import train_test_split
 ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages, net_worths, test_size=0.1, random_state=42)
 
-# ## fill in a regression here!  Name the regression object reg so that
-# ## the plotting code below works, and you can see what your regression looks like
+### fill in a regression here!  Name the regression object reg so that
+### the plotting code below works, and you can see what your regression looks like
 
-reg = LinearRegression().fit(ages_train, net_worths_train)
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
-print(reg.coef_)
+
+
+
+
+
+
 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
@@ -39,11 +45,14 @@ plt.show()
 
 
 ### identify and remove the most outlier-y points
-cleaned_data = []*100
-predictionNet_Worths = reg.predict(net_worths_train)
-cleaned_data = outlierCleaner(predictionNet_Worths, ages_train, net_worths_train)
+cleaned_data = []
+try:
+    predictions = reg.predict(ages_train)
+    cleaned_data = outlierCleaner( predictions, ages_train, net_worths_train )
+except NameError:
+    print("Your regression object doesn't exist, or isn't name reg")
+    print("Can't make predictions to use in identifying outliers")
 
-print(cleaned_data)
 
 
 
@@ -52,30 +61,25 @@ print(cleaned_data)
 
 ### only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
-    ages, net_worths, errors = list(zip(*cleaned_data))
+    ages, net_worths, errors = zip(*cleaned_data)
     ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
     net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
-    ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages, net_worths, test_size=0.1,
-                                                                                random_state=42)
 
     ### refit your cleaned data!
     try:
-        reg.fit(ages_train, net_worths_train)
-        plt.plot(ages_train, reg.predict(ages_train), color="blue")
+        reg.fit(ages, net_worths)
+        plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
-        print("you don't seem to have regression imported/created,")
+        print("You don't seem to have regression imported/created,")
         print("   or else your regression object isn't named reg")
         print("   either way, only draw the scatter plot of the cleaned data")
-
+    
     plt.scatter(ages, net_worths)
     plt.xlabel("ages")
     plt.ylabel("net worths")
     plt.show()
+
 else:
     print("outlierCleaner() is returning an empty list, no refitting to be done")
-
-
-
-
 
 
