a0e247930a1f96739a26f4c731c3d8ea79a5395d
darkbake-WGU <78395093+darkbake-WGU@users.noreply.github.com>
2022-12-23 21:44:27 -0800

    Add files via upload

--- vectorize_text.py
+++ vectorize_text.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
 
+
+#!/usr/bin/python
+
 import os
-import joblib
+import pickle
 import re
 import sys
-import os
 
-sys.path.append(os.path.abspath("../tools/"))
+sys.path.append( "../tools/" )
 from parse_out_email_text import parseOutText
 
-"""
-    Starter code to process the emails from Sara and Chris to extract
-    the features and get the documents ready for classification.
+#from nltk.corpus import stopwords
+#sw = stopwords.words("english")
 
-    The list of all the emails from Sara are in the from_sara list
+
+"""
+    starter code to process the emails from Sara and Chris to extract
+    the features and get the documents ready for classification
+    the list of all the emails from Sara are in the from_sara list
     likewise for emails from Chris (from_chris)
-
-    The actual documents are in the Enron email dataset, which
-    you downloaded/unpacked in Part 0 of the first mini-project. If you have
-    not obtained the Enron email corpus, run startup.py in the tools folder.
-
-    The data is stored in lists and packed away in pickle files at the end.
+    the actual documents are in the Enron email dataset, which
+    you downloaded/unpacked in Part 0 of the first mini-project
+    the data is stored in lists and packed away in pickle files at the end
 """
-print(os.getcwd())
-print(os.listdir('.'))
+
 
 from_sara  = open("from_sara.txt", "r")
 from_chris = open("from_chris.txt", "r")
@@ -34,42 +35,58 @@ word_data = []
 ### temp_counter is a way to speed up the development--there are
 ### thousands of emails from Sara and Chris, so running over all of them
 ### can take a long time
-### temp_counter helps you only look at the first 200 emails in the list so you
-### can iterate your modifications quicker
+### temp_counter helps you only look at the first 200 emails in the list
 temp_counter = 0
 
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
-        ### only look at first 200 emails when developing
-        ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-	        path = os.path.join('..', path[:-1])
-	        print(path)
-	        email = open(path, "r")
-	        text = parseOutText(email)
-        if temp_counter < 200:
+        # only look at first 100 emails
+        if temp_counter < 100:
+            path = os.path.join('..', path[:-1])
+            #print path
+            print(os.getcwd())
+            print(path)
+            email = open(path, "r")
+
+            # use parseOutText to extract the text from the opened email
+            words = parseOutText(email)
+
+            # use str.replace() to remove any instances of the words
+            # ["sara", "shackleton", "chris", "germani"]
+            
             for stop in ["sara", "shackleton", "chris", 
-                         "germani"]:
-                text = text.replace(stop,'')
-        ### append the text to word_data
-                word_data.append(text)
-        
-        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-                if name == "sara":
-                    from_data.append(0)
-                else:
-                    from_data.append(1)
+                         "germani", "sshacklensf", "cgermannsf"]:
+                words = words.replace(stop,'')
+    
+            #Append text to word_data
+                                
+            word_data.append(words)
 
-email.close()
+            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            if name == "sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
-print("Emails Processed")
+
+            email.close()
+
+print ("emails processed")
 from_sara.close()
 from_chris.close()
 
-joblib.dump( word_data, open("your_word_data.pkl", "wb") )
-joblib.dump( from_data, open("your_email_authors.pkl", "wb") )
+pickle.dump( word_data, open("your_word_data.pkl", "w") )
+pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+
+print (word_data[0])
+print (from_data[0])
+
 
 
 ### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words="english")
+print (vectorizer.fit_transform(word_data))
+
+#print len(vectorizer.get_feature_names())
